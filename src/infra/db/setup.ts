b26dd57ee5eaea0c7715ce2ac9cfe9fbdd7e05bf
aida <aida.denisa@gmail.com>
2024-07-13 14:02:24 +0300

    Update TypeORM DataSource Logging

--- src/infra/db/setup.ts
+++ src/infra/db/setup.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata'
-import { DataSource } from 'typeorm'
+import { DataSource, type LoggerOptions } from 'typeorm'
 
 const entitiesPath = process.env.NODE_ENV === 'prod' ? './**/*.entity.js' : './**/*.entity.ts'
 const migrationsPath = process.env.NODE_ENV === 'prod' ? './**/migrations/*.js' : './**/migrations/*.ts'
+const enableLogging: LoggerOptions = process.env.NODE_ENV === 'dev' ? ['query', 'error', 'schema', 'warn', 'info', 'log'] : ['error']
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -12,7 +13,7 @@ export const AppDataSource = new DataSource({
   password: 'S3cret',
   database: 'ncs_db',
   synchronize: false,
-  logging: ['query', 'error', 'schema', 'warn', 'info', 'log'], // Enable detailed logging
+  logging: enableLogging,
   entities: [entitiesPath],
   subscribers: [],
   migrationsTableName: 'migrations',
